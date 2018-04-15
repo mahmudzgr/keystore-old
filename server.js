5d3e6accc20a34e5b1c0000fb8d6287c7718e0c3
Abdülkadir Gökce <gokceakadir@gmail.com>
2018-04-15 22:41:29 +0300

    Project-X Step:1

--- server.js
+++ server.js
@@ -2,6 +2,9 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
+var users = 0;
+var array1 = ['a', 'b' ,'c', 'd'];
+
 app.get('/', function (req, res) {
 	res.sendFile(path.join(__dirname, 'public/index.html'));
 });
@@ -10,6 +13,19 @@ app.get('/akg', function (req, res) {
 	console.log(req);
 	res.end('hello world!');
 });
+
+
+app.get("/mahmud",(req, res) =>{
+	++users;
+	var number= [];
+	var pass="";
+	for(var i=0; i< 16 ;i++){
+	pass += array1[Math.floor(Math.random()*array1.length)];
+	}
+	res.end(pass);
+});
+
+
 
 app.listen(3000, function () {
 	console.log('KeyStore server listening on port 3000!');
