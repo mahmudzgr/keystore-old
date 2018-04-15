2b91916041e120db4ce6e0ebf44ddcd1d79f5501
Abdülkadir Gökce <gokceakadir@gmail.com>
2018-04-15 23:20:24 +0300

    Project-X Step:1.1

--- server.js
+++ server.js
@@ -3,7 +3,25 @@ const app = express();
 const path = require('path');
 
 var users = 0;
-var array1 = ['a', 'b' ,'c', 'd'];
+//var array1 = ['a', 'b' ,'c', 'd', 'e', 'f', 'g' , 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+var lowChar = "abcdefghijklmnopqrstvwxyz";
+var upChar = lowChar.toUpperCase();
+var num = "0123456789";
+var characters = ".,/?!;'[]{}()*&^%$#@`~<>|";
+
+ var randPass = function (){
+	var select = Math.floor(Math.random()*20);
+	if(select>=0 && select<7){
+		return lowChar[Math.floor(Math.random()*lowChar.length)];
+	}else if(select>=7 && select<14){
+		return upChar[Math.floor(Math.random()*upChar.length)];
+	}else if(select>=14 && select <18){
+		return num[Math.floor(Math.random()*num.length)];
+	}else{
+		return characters[Math.floor(Math.random()*characters.length)];
+	}
+};
+
 
 app.get('/', function (req, res) {
 	res.sendFile(path.join(__dirname, 'public/index.html'));
@@ -20,7 +38,7 @@ app.get("/mahmud",(req, res) =>{
 	var number= [];
 	var pass="";
 	for(var i=0; i< 16 ;i++){
-	pass += array1[Math.floor(Math.random()*array1.length)];
+	pass += randPass();
 	}
 	res.end(pass);
 });
