85b589edd5639dad6e19c837031d18bd85b118f2
Abdülkadir Gökce <gokceakadir@gmail.com>
2018-04-18 23:09:53 +0300

    Project-X Step:1.3

--- server.js
+++ server.js
@@ -9,13 +9,51 @@ var upChar = lowChar.toUpperCase();
 var num = "0123456789";
 var characters = ".,/?!;'[]{}()*&^%$#@`~<>|";
 
- var randPass = function (chars,digits){
+
+/*
+class Password{
+
+	constructor(length,digits,chars){
+
+		var odds =  "";
+		for(var i=0; i<8; i++){odds += '0'};
+		for(var i=0; i<8; i++){odds += '1'}; // Can we remevo this recurring code somehow?
+
+		if(digits === "on"){for(var i=0; i<3; i++){odds += '2'}}
+	 	if(chars === "on"){for(var i=0; i<1; i++){odds += '3'}}
+
+		var select = Math.floor(Math.random()*odds.length);
+
+		this.pass="";
+
+		for(var i=0; i<length; i++){
+			if(odds[select]==='0'){ this.pass + lowChar[Math.floor(Math.random()*lowChar.length)];
+			}else if(odds[select]==='1'){ this.pass + upChar[Math.floor(Math.random()*upChar.length)];
+			}else if(odds[select]==='2'){ this.pass + num[Math.floor(Math.random()*num.length)];
+			}else{ this.pass + characters[Math.floor(Math.random()*characters.length)];}
+		}
+
+		for(var i=0; i<length; i++){
+			if(odds[select]==='0'){ this.pass.push(lowChar[Math.floor(Math.random()*lowChar.length)]);
+			}else if(odds[select]==='1'){ this.pass.push(upChar[Math.floor(Math.random()*upChar.length)]);
+			}else if(odds[select]==='2'){ this.pass.push(num[Math.floor(Math.random()*num.length)]);
+			}else{ this.pass.push(characters[Math.floor(Math.random()*characters.length)]);}
+		}
+	}
+
+}; 
+*/
+
+
+ var randPass = function (digits,chars){
 
  	var odds =  "";
- 	for(var i=0; i<8; i++){odds += '0'};
- 	for(var i=0; i<8; i++){odds += '1'};
- 	if(digits === "true"){for(var i=0; i<3; i++){odds += '2'}}
- 	if(chars === "true"){for(var i=0; i<1; i++){odds += '3'}}
+	for(var i=0; i<8; i++){odds += '0'};
+	for(var i=0; i<8; i++){odds += '1'}; // Can we remevo this recurring code somehow?
+
+
+ 	if(digits === "on"){for(var i=0; i<3; i++){odds += '2'}}
+ 	if(chars === "on"){for(var i=0; i<1; i++){odds += '3'}}
 
 	var select = Math.floor(Math.random()*odds.length);
 
@@ -50,10 +88,12 @@ app.get("/api/password", (req,res)=>{
 	++users;
 
 	var length = req.query.length;
-	var chars = req.query.chars;
 	var digits = req.query.digits;
+	var chars = req.query.chars;
 	var pass="";
-	for(var i=0; i<length; i++){ pass += randPass(chars,digits);}
+	for(var i=0; i<length; i++){ pass += randPass(digits,chars);}
+
+	//const new_password = new Password(length,chars,digits);
 
 	res.type('text');
 	res.end(pass);
